65c551f0ab34574a36b6443d6d6a9dc2b47a6ae8
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2023-02-17 02:17:42 +0300

    update on middleware

--- Homework_1/Middlewares/GlobalLoggingMiddleware.cs
+++ Homework_1/Middlewares/GlobalLoggingMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Homework_1.Middlewares
@@ -6,17 +7,28 @@ namespace Homework_1.Middlewares
     public class GlobalLoggingMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
 
-        public GlobalLoggingMiddleware(RequestDelegate next)
+        public GlobalLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
             _next = next;
+            _logger = loggerFactory.CreateLogger<GlobalLoggingMiddleware>();
         }
 
         public async Task Invoke(HttpContext httpContext)
         {
-            string message = "Logging is successfull.";
-            System.Console.WriteLine(message);
-            await _next(httpContext);
+            try
+            {
+                await _next(httpContext);
+            }
+            finally
+            {
+                _logger.LogInformation(
+                    "Request {method} {url} => {statusCode}",
+                    httpContext.Request?.Method,
+                    httpContext.Request?.Path.Value,
+                    httpContext.Response?.StatusCode);
+            }
         }
     }
 }
