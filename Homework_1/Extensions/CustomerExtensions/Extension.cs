d120e7ea02828e39c620edf99fb39ac3b56e4a7f
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2023-02-17 01:30:21 +0300

    extensions added

---
+++ Homework_1/Extensions/CustomerExtensions/Extension.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+using Homework_1.Entities;
+
+namespace Homework_1.Extensions.CustomerExtensions
+{
+    public static class Extension
+    {
+        // Bu metod ile bir öğe (item)’in koleksiyon içerisinde olup olmadığını kontrol etmek için kullanılabilir.
+        public static bool In<T>(this T value, params T[] list)
+        {
+            if (null == value) throw new ArgumentNullException("value");
+            return list.Contains(value);
+        }
+
+        //koleksiyon içerisinde tekrar eden kayıtları silmenize yarayacak başla bir extension metod
+
+        public static List<Customer> RemoveDuplicates<T>(this List<Customer> input)
+        {
+            Dictionary<Customer, int> uniqueStore = new Dictionary<Customer, int>();
+            List<Customer> finalList = new List<Customer>();
+
+            foreach (Customer currValue in input)
+            {
+                if (!uniqueStore.ContainsKey(currValue))
+                {
+                    uniqueStore.Add(currValue, 0);
+                    finalList.Add(currValue);
+                }
+            }
+            return finalList;
+        }
+
+    }
+}
